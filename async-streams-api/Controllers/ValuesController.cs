5116a5f035d52ac1f4f7079450bbf5f2acef4f9f
Muhammed Saleem <Muhammed.P@gds.ey.com>
2021-08-21 22:27:14 +0530

    initial commit

---
+++ async-streams-api/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace async_streams_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IAsyncEnumerable<int> Get()
+        {
+            return GetNumberStreams();
+        }
+
+        private async IAsyncEnumerable<int> GetNumberStreams()
+        {
+            int index = 0;
+            while (true)
+                yield return index++;
+        }
+    }
+}
